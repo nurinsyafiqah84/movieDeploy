de6bb6d76c2f7826e18ea35b1cd8ee65764664fb
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-14 13:44:19 +0800

    banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <div>
       <Header/>
+      <div className='max-w-[1300px] mx-auto'>
+
+      </div>
       <footer>
         <Footer/>
       </footer>
