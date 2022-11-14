ed056be2b9278fff58e09c499e18b7bd83722b4b
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-14 10:19:55 +0800

    updated 14/11/2022

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images:{
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages", 
+}
+
+module.exports = nextConfig
