6459db5c39f02c00f29d159c52c9b07448b0e64a
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-14 16:47:49 +0800

    banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Container from "./layouts/Container";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Footer/>
+      <Container>
+        <Banner/>
+      </Container>
     </div>
   )
 }
