849bcac52107c9ed7b77ca33de7c048730ba6dfd
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-15 13:14:33 +0800

    movielist

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,9 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieList from "./components/Movies/MovieList";
 import Container from "./layouts/Container";
-import Section from "./layouts/Section";
 
 export default function Home() {
-  const handleToggledValue = (selectedValue: any) =>{
-    console.log(selectedValue)
-  }
+  
   return (
     <div>
       <Header/>
@@ -16,9 +13,8 @@ export default function Home() {
         <Banner/>
         {/*another contoh example props
         <Section props="What's popular"/> */}
-        <Section title="What's popular" items={["Streaming", "On TV", "For Rent", "In Theatres"]} onToggle={handleToggledValue}>
-          <MovieList/>
-        </Section>
+        <MovieList/>
+        
       </Container>
       <Footer/>
     </div>
