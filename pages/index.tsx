1ef6c479a816eb52b9cb52c7bf238121b38cd969
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-15 10:46:01 +0800

    toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,21 @@ import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Container from "./layouts/Container";
+import Section from "./layouts/Section";
 
 export default function Home() {
+  const handleToggledValue = (selectedValue: any) =>{
+    console.log(selectedValue)
+  }
   return (
     <div>
       <Header/>
       <Container>
         <Banner/>
+        {/*another contoh example props
+        <Section props="What's popular"/> */}
+        <Section title="What's popular" items={["Streaming", "On TV", "For Rent", "In Theatres"]} onToggle={handleToggledValue}>
+        </Section>
       </Container>
       <Footer/>
     </div>
