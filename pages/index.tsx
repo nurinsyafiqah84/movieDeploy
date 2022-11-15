28c926c7d82c98944c7ad37c1a85f3c1fcf2c348
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-15 11:21:34 +0800

    toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import MovieList from "./components/Movies/MovieList";
 import Container from "./layouts/Container";
 import Section from "./layouts/Section";
 
@@ -16,6 +17,7 @@ export default function Home() {
         {/*another contoh example props
         <Section props="What's popular"/> */}
         <Section title="What's popular" items={["Streaming", "On TV", "For Rent", "In Theatres"]} onToggle={handleToggledValue}>
+          <MovieList/>
         </Section>
       </Container>
       <Footer/>
