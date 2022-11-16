6fbd404f830df62ba2b5720a2776ad74faf5bea6
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-16 12:53:11 +0800

    toggle test

--- pages/baseUI/Switch/index.jsx
+++ pages/baseUI/Switch/index.jsx
@@ -1,12 +1,42 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 const Switch = ({items}) => {
     
     if (Array.isArray(items)){
         console.log(items[0])
     }
 
+    const [isToggled, setToggle] = useState(false);
+    const [item1Width, setItem1Width] = useState();
+    const [item2Width, setItem2Width] = useState();
+    const item1Ref = useRef();
+    const item2Ref = useRef();
+
+    const handleToggle = (toggleState) => {
+        setToggle(toggleState)
+    }
+
+    useLayoutEffect(() =>{
+        setItem1Width(item1Ref.current.offsetWidth)
+        setItem2Width(item2Ref.current.offsetWidth) 
+    },[])
+
+    const activeTextColor = "bg-clip-text text-transparent text-green bg-gradient-to-r from-[#c0fecf] to-[#1ed5a9]"
+
     return (
-        <div>
-            {items?.[0]}
+        <div className="hover: cursor-pointer h-8 border-solid border-darkBlue rounded-[30px] border-[1px] font-semibold flex items-center relative">
+            {/*{items?.[0]}*/}
+            <div ref={item1Ref} onClick={handleToggle.bind(null, false)} className={`py-1 px-5 h-8 rounded-[30px] ${isToggled || activeTextColor}`}>{items?.[0]}</div>
+            <div ref={item2Ref} onClick={() => {handleToggle(true)}} className={`py-1 px-5 h-8 rounded-[30px] ${isToggled && activeTextColor}`}>{items?.[1]}</div>
+            <div className="h-8 w-20 bg-darkBlue rounded-[30px] absolute z-[-1] transition-all duration-150 ease-in"style={
+            isToggled?{
+                left:`${item1Width}px`,
+                width:`${item2Width+2}px`
+            }:{
+                left:'0',
+                width:`${item1Width}px`
+            }
+        }></div>
         </div>
     );
 };
