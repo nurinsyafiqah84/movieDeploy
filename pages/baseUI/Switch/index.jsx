519955c9253f829b5dea44e07eb1ccc17c1f8f74
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-16 13:04:13 +0800

    toggle test

--- pages/baseUI/Switch/index.jsx
+++ pages/baseUI/Switch/index.jsx
@@ -18,7 +18,7 @@ const Switch = ({items}) => {
 
     useLayoutEffect(() =>{
         setItem1Width(item1Ref.current.offsetWidth)
-        setItem2Width(item2Ref.current.offsetWidth) 
+        setItem2Width(item2Ref.current.offsetWidth) //done
     },[])
 
     const activeTextColor = "bg-clip-text text-transparent text-green bg-gradient-to-r from-[#c0fecf] to-[#1ed5a9]"
@@ -28,7 +28,15 @@ const Switch = ({items}) => {
             {/*{items?.[0]}*/}
             <div ref={item1Ref} onClick={handleToggle.bind(null, false)} className={`py-1 px-5 h-8 rounded-[30px] ${isToggled || activeTextColor}`}>{items?.[0]}</div>
             <div ref={item2Ref} onClick={() => {handleToggle(true)}} className={`py-1 px-5 h-8 rounded-[30px] ${isToggled && activeTextColor}`}>{items?.[1]}</div>
-            
+            <div className="h-8 w-20 bg-darkBlue rounded-[30px] absolute z-[-1] transition-all duration-150 ease-in"style={
+            isToggled?{
+                left:`${item1Width}px`,
+                width:`${item2Width+2}px`
+            }:{
+                left:'0',
+                width:`${item1Width}px`
+            }
+        }></div>
         </div>
     );
 };
--- pages/layouts/Section/index.jsx
+++ pages/layouts/Section/index.jsx
@@ -2,8 +2,8 @@ import Switch from "../../baseUI/Switch";
 
 const Section = ({title, items}) => {
     return (
-        <section className='pt-[30px] px-10'>
-            <div>
+        <section className='pt-[30px]'>
+            <div className='flex items-center gap-5 px-10'>
                 <h2 className='font-semibold text-2xl'>{title}</h2>
                 <Switch items={items}/>
                 
