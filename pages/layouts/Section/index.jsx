1328deb7e780ce7d0747f814de689982b0fd0867
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-16 11:39:32 +0800

    toggle test

--- pages/layouts/Section/index.jsx
+++ pages/layouts/Section/index.jsx
@@ -5,7 +5,7 @@ const Section = ({title, items}) => {
         <section className='pt-[30px] px-10'>
             <div>
                 <h2 className='font-semibold text-2xl'>{title}</h2>
-                <Switch/>
+                <Switch items={items}/>
             </div>
         </section>
     );
