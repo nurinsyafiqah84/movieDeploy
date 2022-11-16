07ce5ed0b10375cdc8e1414876badfc1dff79fa7
Nurin Syafiqah <nmuslimah8@gmail.com>
2022-11-16 12:58:41 +0800

    toggle test

--- pages/baseUI/Switch/index.jsx
+++ pages/baseUI/Switch/index.jsx
@@ -28,15 +28,7 @@ const Switch = ({items}) => {
             {/*{items?.[0]}*/}
             <div ref={item1Ref} onClick={handleToggle.bind(null, false)} className={`py-1 px-5 h-8 rounded-[30px] ${isToggled || activeTextColor}`}>{items?.[0]}</div>
             <div ref={item2Ref} onClick={() => {handleToggle(true)}} className={`py-1 px-5 h-8 rounded-[30px] ${isToggled && activeTextColor}`}>{items?.[1]}</div>
-            <div className="h-8 w-20 bg-darkBlue rounded-[30px] absolute z-[-1] transition-all duration-150 ease-in"style={
-            isToggled?{
-                left:`${item1Width}px`,
-                width:`${item2Width+2}px`
-            }:{
-                left:'0',
-                width:`${item1Width}px`
-            }
-        }></div>
+            
         </div>
     );
 };
